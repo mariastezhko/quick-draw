d10a08367860c0f44b6d24f3418d973080d23377
Maria Stezhko <stezhko@gmail.com>
2018-03-08 15:11:05 -0800

    Initial commit

---
+++ QuickDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickDraw
+//
+//  Created by Maria Stezhko on 3/8/18.
+//  Copyright © 2018 QuickDraw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
